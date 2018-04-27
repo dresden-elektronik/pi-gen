8137a6ca08887934feb205b261520e8a555a9f6c
ChrisHae <cha@dresden-elektronik.de>
2018-04-27 11:27:59 +0200

    changed local deconz repository to online deconz repository in apt sources.list

--- stage0/00-configure-apt/00-run.sh
+++ stage0/00-configure-apt/00-run.sh
@@ -3,15 +3,12 @@
 install -m 644 files/sources.list "${ROOTFS_DIR}/etc/apt/"
 install -m 644 files/raspi.list "${ROOTFS_DIR}/etc/apt/sources.list.d/"
 
-cp -r files/deconz_repo/ "${ROOTFS_DIR}/tmp/"
-
 if [ -n "$APT_PROXY" ]; then
 	install -m 644 files/51cache "${ROOTFS_DIR}/etc/apt/apt.conf.d/51cache"
 	sed "${ROOTFS_DIR}/etc/apt/apt.conf.d/51cache" -i -e "s|APT_PROXY|${APT_PROXY}|"
 else
 	rm -f "${ROOTFS_DIR}/etc/apt/apt.conf.d/51cache"
 fi
-
 
 on_chroot apt-key add - < files/raspberrypi.gpg.key
 on_chroot apt-key add - < files/deconz.pub.key
